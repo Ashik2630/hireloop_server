3353364c58cd907417315c63a8d0c56a4a2d787a
Ashik2630 <ashik426370@gmail.com>
2026-06-18 23:20:17 +0800

    updaaaaate file

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const express = require('express');
-const app = express()
-const cors = require('cors')
-const port = process.env.PORT || 5000
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const uri = process.env.MONGODB_URI;
 
@@ -23,48 +20,87 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    
-    const database = client.db('hireloopDB');
-    const jobsCollection = database.collection('jobs');
 
+    const database = client.db("hireloopDB");
+    const jobsCollection = database.collection("jobs");
+    const companyCollection = database.collection("companies");
+    const userCollection = database.collection("user");
 
-    app.get('/api/jobs', async (req, res)=> {
+    app.get("/api/users", async (req, res) => {
+
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.json(result || {});
+    })
+
+    app.get("/api/jobs", async (req, res) => {
       const query = {};
-      
-      if(req.query.companyId){
+
+      if (req.query.companyId) {
         query.companyId = req.query.companyId;
       }
 
-      if(req.query.status){
+      if (req.query.status) {
         query.status = req.query.status;
       }
 
       const cursor = jobsCollection.find(query);
       const result = await cursor.toArray();
-      res.json(result);
-    })
+      // console.log(result);
+      res.json(result || {});
+    });
 
-
-    app.post('/api/jobs', async (req, res)=> {
+    app.post("/api/jobs", async (req, res) => {
       const job = req.body;
-      const result = await jobsCollection.insertOne(job);
+      const newJob = {
+        ...job,
+        createdAt: new Date(),
+      }
+      const result = await jobsCollection.insertOne(newJob);
       res.json(result);
+    });
+
+    // company related api
+
+    app.get("/api/companies", async (req, res) => {
+      const cursor = companyCollection.find();
+      const result = await cursor.toArray();
+      res.json(result || {});
     })
 
 
+    app.get("/api/my/companies", async (req, res) => {
+      const query = {};
+      if (req.query.recruiterId) {
+        query.recruiterId = req.query.recruiterId;
+      }
 
+      const result = await companyCollection.findOne(query);
+      res.json(result || {});
+    });
 
+    app.post("/api/companies", async (req, res) => {
+      const company = req.body;
+      const newCompany = {
+        ...company,
+        createdAt: new Date(),
+      }
+      const result = await companyCollection.insertOne(newCompany);
+      res.json(result);
+    });
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -72,9 +108,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
